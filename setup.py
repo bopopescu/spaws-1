080b02e46d376fc8591ecf17418a133d68bb1a2b
Klaas Bosteels <klaas.bosteels@gmail.com>
2014-11-20 15:13:42 +0100

    Make spark-ec2 top-level entry point

--- setup.py
+++ setup.py
@@ -1,20 +1,11 @@
 from setuptools import setup, find_packages
 
 
-name = "spaws"
-version = "0.1"
-requires = ["setuptools", "boto", "click"]
-
-
-###########################################
-# probably no need to edit anything below #
-###########################################
-
 kwargs = {
-    "name": name,
-    "version": version,
+    "name": "spaws",
+    "version": "0.1",
     "packages": find_packages(exclude=["tests", "tests.*"]),
-    "install_requires": requires,
+    "install_requires": ["setuptools", "boto", "click"],
     "test_suite": "nose.collector",
     "tests_require": ["nose"],
     "include_package_data": True
@@ -23,10 +14,11 @@ kwargs["entry_points"] = {
     # buildout.cfg should be used instead to define entry points,
     # unless they are suitable to be installed globally
     "console_scripts": [
-        name + " = " + name + ":main"
+        "spaws = spaws:main",
+        "spark-ec2 = spaws.spark_ec2:main"
     ],
     "setuptools.installation": [
-        "eggsecutable = " + name + ":main",
+        "eggsecutable = spaws:main",
     ]
 }
 setup(**kwargs)
